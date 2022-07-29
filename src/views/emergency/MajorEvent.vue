4f4279dc3f5d2a38324bf8a9c44e744284f07f2b
xiaobin.ma <928975247@qq.com>
2022-07-29 14:03:56 +0800

    更新重大活动图片

--- src/views/emergency/MajorEvent.vue
+++ src/views/emergency/MajorEvent.vue
@@ -153,7 +153,9 @@ export default {
         },
         changeWorkArea(scene) {
             this.curscene = scene;
-            if (this.curscene === "客流-线") {
+            if (this.curscene === "客流-网") {
+                this.workurl = `http://172.51.216.64/sdss/tc.html?name=客流-网2.png`;
+            } else if (this.curscene === "客流-线") {
                 if (this.curflow === "方案结束判断") {
                     this.workurl = `http://bi.funenc.com/bi/?proc=1&action=viewer&hback=true&db=!5317!!4eac!!8f68!!9053!!4ea4!!901a!!5927!!8bfe!!9898!!2f!!65e5!!5e38!!6a21!!5f0f!-!5ba2!!6d41!!7ef4!!5ea6!-!533a!!95f4!.db&platform=PC&browserType=chrome&au_act=login&adminv=aoms&passv=g5`;
                     // this.workurl = `http://172.51.216.64/sdss/tc.html?name=方案结束判断1.png`;
@@ -162,7 +164,7 @@ export default {
                 }
             } else if (scene === "客流-站") {
                 // this.workurl = `http://bi.funenc.com/bi/?proc=1&action=viewer&hback=true&db=!5317!!4eac!!8f68!!9053!!4ea4!!901a!!5927!!8bfe!!9898!!2f!!65e5!!5e38!!6a21!!5f0f!-!5ba2!!6d41!!7ef4!!5ea6!-!67a2!!7ebd!!7ad9!.db&platform=PC&browserType=chrome&au_act=login&adminv=aoms&passv=g5`;
-                 this.workurl = `http://bi.funenc.com/bi/?proc=1&action=viewer&hback=true&db=!5317!!4eac!!8f68!!9053!!4ea4!!901a!!5927!!8bfe!!9898!!2f!!65e5!!5e38!!6a21!!5f0f!-!5ba2!!6d41!!7ef4!!5ea6!-!6362!!4e58!!7ad9!!5ba2!!6d41!.db&platform=PC&browserType=chrome&au_act=login&adminv=aoms&passv=g5`;
+                this.workurl = `http://bi.funenc.com/bi/?proc=1&action=viewer&hback=true&db=!5317!!4eac!!8f68!!9053!!4ea4!!901a!!5927!!8bfe!!9898!!2f!!65e5!!5e38!!6a21!!5f0f!-!5ba2!!6d41!!7ef4!!5ea6!-!6362!!4e58!!7ad9!!5ba2!!6d41!.db&platform=PC&browserType=chrome&au_act=login&adminv=aoms&passv=g5`;
             } else {
                 this.workurl = `http://172.51.216.64/sdss/tc.html?name=${scene}.png`;
             }
