ca70f14e4630c88bdc646041702fe2c1af8904f3
Eels <cloted@hotmail.com>
2022-09-29 16:26:56 +0800

    菜单调整

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,8 @@
                     </el-menu-item>
                 </el-submenu>
             </el-menu> -->
+            
+            
 
             <el-menu :default-active="activeIndex"
                 class="el-menu-demo"
@@ -53,39 +55,53 @@
                 <el-submenu index="1">
                     <template slot="title">线网运营监察</template>
                     <el-menu-item index="1-1">
-                        <router-link to="/line/comprehensive">线网综合图</router-link>
+                        <router-link to="/line/comprehensive">综合线网图</router-link>
                     </el-menu-item>
                     <el-menu-item index="1-2">
-                        <router-link to="/line/passengerflow">客流信息</router-link>
+                        <router-link to="/line/passengerflow">客流监控</router-link>
                     </el-menu-item>
                     <el-menu-item index="1-3">
-                        <router-link to="/line/driving">行车信息</router-link>
+                        <router-link to="/line/driving">行车监控</router-link>
                     </el-menu-item>
                     <el-menu-item index="1-4">
-                        <router-link to="/line/energy">能耗信息</router-link>
+                        <router-link to="/line/energy">能耗监控</router-link>
                     </el-menu-item>
                     <el-menu-item index="1-5">
                         <router-link to="/line/monitor">设备监视</router-link>
                     </el-menu-item>
                     <el-menu-item index="1-6">
-                        <router-link to="/line/alarm">报警信息</router-link>
+                        <router-link to="/cctv/preview">CCTV实时监控</router-link>
                     </el-menu-item>
                     <el-menu-item index="1-7">
-                        <router-link to="/line/plane">车站平面图</router-link>
+                        <router-link to="/line/alarm">报警及处置</router-link>
                     </el-menu-item>
                     <el-menu-item index="1-8">
-                        <router-link to="/tool/important">重保活动</router-link>
+                        <router-link to="/line/plane">平面图</router-link>
                     </el-menu-item>
+                    <!-- <el-menu-item index="1-8">
+                        <router-link to="/tool/important">重保活动</router-link>
+                    </el-menu-item> -->
                 </el-submenu>
                 <el-submenu index="2">
                     <template slot="title">平行推演</template>
                     <!-- <el-menu-item index="2-1">
                         <router-link to="/choseline">常规运营</router-link>
                     </el-menu-item> -->
+
+                    <el-submenu index="2-1">
+                        <template slot="title">离线推演</template>
+                        <el-menu-item index="2-1-1">
+                            <router-link to="/choseline">开行方案验证</router-link>
+                        </el-menu-item>
+                        <el-menu-item index="2-1-2">
+                            <router-link to="/offlinescheme">运行图比选</router-link>
+                        </el-menu-item>
+                    </el-submenu>
+
                     <el-menu-item index="2-2">
-                        <router-link to="/offlinescheme">离线方案对比</router-link>
+                        <router-link to="/line/comprehensive">在线推演</router-link>
                     </el-menu-item>
-                    <el-menu-item index="2-3">
+                    <!-- <el-menu-item index="2-3">
                         <router-link to="/emergency/rainstorm">暴雨机场大面积航班延误</router-link>
                     </el-menu-item>
                     <el-menu-item index="2-4">
@@ -93,19 +109,45 @@
                     </el-menu-item>
                     <el-menu-item index="2-5">
                         <router-link to="/emergency/daily">常规运营</router-link>
-                    </el-menu-item>
+                    </el-menu-item> -->
                 </el-submenu>
                 <el-submenu index="3">
-                    <template slot="title">系统态势研判</template>
+                    <template slot="title">态势研判</template>
                     <!-- <el-menu-item index="3-1">
                         <router-link to="/monitor">系统综合评价指标</router-link>
-                    </el-menu-item> -->
+                    </el-menu-item>
                     <el-menu-item index="3-2">
                         <router-link to="/situation/detailed">专业性指标详细展示</router-link>
                     </el-menu-item>
                     <el-menu-item index="3-3">
                         <router-link to="/situation/trend">指标趋势分析</router-link>
+                    </el-menu-item> -->
+
+                    <el-menu-item index="3-1">
+                        <router-link to="/line/comprehensive">综合评价</router-link>
                     </el-menu-item>
+                    <el-menu-item index="3-2">
+                        <router-link to="/line/passengerflow">态势评估</router-link>
+                    </el-menu-item>
+                    <el-submenu index="3-3">
+                        <template slot="title">趋势分析</template>
+                        <el-menu-item index="3-3-1">
+                            <router-link to="/line/comprehensive">客流指标</router-link>
+                        </el-menu-item>
+                        <el-menu-item index="3-3-2">
+                            <router-link to="/line/comprehensive">行车指标</router-link>
+                        </el-menu-item>
+                        <el-menu-item index="3-3-3">
+                            <router-link to="/line/comprehensive">能耗指标</router-link>
+                        </el-menu-item>
+                        <el-menu-item index="3-3-4">
+                            <router-link to="/line/comprehensive">设备健康指标</router-link>
+                        </el-menu-item>
+                    </el-submenu>
+                    <el-menu-item index="3-4">
+                        <router-link to="/system/indicator">指标配置管理</router-link>
+                    </el-menu-item>
+
                 </el-submenu>
                 <el-submenu index="4">
                     <template slot="title">综合决策优化</template>
@@ -116,44 +158,79 @@
                         <router-link to="/policy/passengerflow">客流辅助决策</router-link>
                     </el-menu-item>
                     <el-menu-item index="4-3">
-                        <router-link to="/policy/people">人员物资辅助</router-link>
+                        <router-link to="/policy/people">人员物资辅助决策</router-link>
                     </el-menu-item>
-                    <el-menu-item index="4-4">
+                    <!-- <el-menu-item index="4-4">
                         <router-link to="/policy/assessment">重大事件后评估</router-link>
-                    </el-menu-item>
+                    </el-menu-item> -->
                 </el-submenu>
                 <el-submenu index="5">
-                    <template slot="title">应急会商</template>
-                    <el-menu-item index="5-1">
+                    <template slot="title">事件处置</template>
+                    <!-- <el-menu-item index="5-1">
                         <router-link to="/consultation/management">应急处置</router-link>
                     </el-menu-item>
                     <el-menu-item index="5-2">
                         <router-link to="/consultation/soldier">应急单兵接入</router-link>
+                    </el-menu-item> -->
+
+                    <el-menu-item index="5-1">
+                        <router-link to="/choseline">预案编制</router-link>
                     </el-menu-item>
+                    <el-menu-item index="5-2">
+                        <router-link to="/tool/important">活动时间表</router-link>
+                    </el-menu-item>
+                    <el-submenu index="5-3">
+                        <template slot="title">活跃事件</template>
+                        <el-menu-item index="5-3-1">
+                            <router-link to="/emergency/rainstorm">暴雨0906</router-link>
+                        </el-menu-item>
+                        <el-menu-item index="5-3-1">
+                            <router-link to="/emergency/majorevent">跨年夜1231</router-link>
+                        </el-menu-item>
+                    </el-submenu>                    
+                    <el-menu-item index="5-4">
+                        <router-link to="/consultation/soldier">应急单兵</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="5-5" disabled>
+                        <router-link to="/consultation/management">应急通信录</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="5-6" disabled>
+                        <router-link to="/consultation/management">应急物资</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="5-7">
+                        <router-link to="/consultation/management">应急会商</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="5-8">
+                        <router-link to="/tool/command">信息与命令</router-link>
+                    </el-menu-item>
+
                 </el-submenu>
-                <el-submenu index="6">
-                    <template slot="title">工具</template>
+                <!-- <el-submenu index="6">
+                    <template slot="title">报表</template>
                     <el-menu-item index="6-1">
                         <router-link to="/tool/command">信息与命令</router-link>
                     </el-menu-item>
-                    <!-- <el-menu-item index="6-2">
+                    <el-menu-item index="6-2">
                         <router-link to="/tool/alarm">报警</router-link>
-                    </el-menu-item> -->
+                    </el-menu-item>
                     <el-menu-item index="6-3">
                         <router-link to="/tool/message">消息</router-link>
                     </el-menu-item>
                     <el-menu-item index="6-5">
                         <router-link to="/tool/report">报表</router-link>
                     </el-menu-item>
-                </el-submenu>
+                </el-submenu> -->
+
+                <el-menu-item index="6"><router-link to="/tool/report">报表</router-link></el-menu-item>
+
                 <el-submenu index="7">
-                    <template slot="title">系统管理</template>
-                    <el-menu-item index="7-1">
+                    <template slot="title">系统配置</template>
+                    <!-- <el-menu-item index="7-1">
                         <router-link to="/system/indicator">指标配置管理</router-link>
                     </el-menu-item>
-                    <!-- <el-menu-item index="7-2">
+                    <el-menu-item index="7-2">
                         <router-link to="/monitor">预案管理</router-link>
-                    </el-menu-item> -->
+                    </el-menu-item>
                     <el-menu-item index="7-3">
                         <router-link to="/system/role">角色权限管理</router-link>
                     </el-menu-item>
@@ -162,9 +239,23 @@
                     </el-menu-item>
                     <el-menu-item index="7-5">
                         <router-link to="/system/about">关于综合决策</router-link>
+                    </el-menu-item> -->
+
+                    <el-menu-item index="7-1">
+                        <router-link to="/tool/message">操作记录</router-link>
                     </el-menu-item>
+                    <el-menu-item index="7-2">
+                        <router-link to="/system/role">用户管理</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="7-3">
+                        <router-link to="/system/function">系统设置</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="7-4">
+                        <router-link to="/system/about">关于</router-link>
+                    </el-menu-item>
+
                 </el-submenu>
-                <el-submenu index="8">
+                <!-- <el-submenu index="8">
                     <template slot="title">CCTV监控</template>
                     <el-menu-item index="8-1">
                         <router-link to="/cctv/marshalling">CCTV编组</router-link>
@@ -172,7 +263,7 @@
                     <el-menu-item index="8-2">
                         <router-link to="/cctv/preview">CCTV实时监控</router-link>
                     </el-menu-item>
-                </el-submenu>
+                </el-submenu> -->
             </el-menu>
             <img class="logo" src="../src/assets/basie/logotop.png" />
         </div>
